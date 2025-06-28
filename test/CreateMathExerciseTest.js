69b821a46d9614b1f606f0324348c6dadff79e56
Personal Laptop <alan1c@hotmail.com>
2025-06-28 16:25:37 -0700

    uncomment code just for testing git

--- test/CreateMathExerciseTest.js
+++ test/CreateMathExerciseTest.js
@@ -20,14 +20,14 @@ describe('cedar math tests', () => {
         });
     });
     
-    // describe('math.multiply() Test', () => {
-    //     it('should equal 3', () => {
-    //         const result = math.multiply(3, 1);
-    //         expect(result).to.equal(3);
-    //     });
-    //     it('should equal 10', () => {
-    //         const result = math.multiply(5, 2);
-    //         expect(result).to.equal(10);
-    //     });
-    // });
+describe('math.multiply() Test', () => {
+     it('should equal 3', () => {
+         const result = math.multiply(3, 1);
+             expect(result).to.equal(3);
+         });
+         it('should equal 10', () => {
+             const result = math.multiply(5, 2);
+             expect(result).to.equal(10);
+         });
+     });
 });
